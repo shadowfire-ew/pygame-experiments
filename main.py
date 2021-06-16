8f130703ade165919db1e922421fcc393d8ec974
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-16 14:24:12 -0400

    starting with the display

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
 import pygame
+pygame.init()
 
+size = width,height = 1080,960
+
+bg = 255,0,0
+
+def main():
+    print("start main")
+    looping = True
+    screen = pygame.display.set_mode(size)
+    while looping:
+        # do stuff
+
+        # update display
+        # do background
+        screen.fill(bg)
+        # fore ground stuff
+        # complete display
+        pygame.display.flip()
+
+        # at the end of the loop
+        for event in pygame.event.get():
+            # checking if we have our exit event
+            looping = (event.type != pygame.QUIT)
+    return
+
+if __name__ == "__main__":
+    main()
