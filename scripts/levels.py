55eedd48a1f94c91377e9399b8044ec57c79f46c
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-18 19:03:38 -0400

    finished the load function, for now

--- scripts/levels.py
+++ scripts/levels.py
@@ -11,4 +11,10 @@ key = {
 class Level(object):
     def load_file(self, filename):
         self.map = []
-        pass
+        parser.read(maps_folder+"filename")
+        self.tileset = parser.get("level","tileset")
+        self.map = parser.get("level","map").split('\n')
+        self.width = len(self.map[0])
+        self.height = len(self.map)
+    
+    def get_tile(self, x, y):
