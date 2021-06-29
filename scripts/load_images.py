705068aae3c0dd227b384b0632c1a43c3103a14b
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-29 16:45:24 -0400

    added a load animations function

--- scripts/load_images.py
+++ scripts/load_images.py
@@ -42,3 +42,21 @@ def load_tile_table(filename, width, height = None):
             line.append(image.subsurface(rect))
         tile_table.append(line)
     return tile_table
+
+def load_animations(filename, width, height = None):
+    """
+    uses a similar method as the tile table loader to load sprites
+    this time it will load them all into one array
+    """
+    if height is None:
+        # in case a height is not included
+        height = width
+
+    image = pygame.image.load(location+'sprites/characters/'+filename).convert()
+    im_width, im_height = image.get_size()
+    animation = []
+    for tile_y in range(0,im_height//height):
+        for tile_x in range(0,im_width//width):
+            rect = (tile_x*width, tile_y*height,width,height)
+            animation.append(image.subsurface(rect))
+    return animation
