70014a61e59f89a4c82ceb3332237a68fc7f0315
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-29 17:36:18 -0400

    character animation code
    could be incomplete

--- scripts/character.py
+++ scripts/character.py
@@ -4,6 +4,7 @@ this file sill describe the general character class
 from configparser import ConfigParser
 import pygame
 import scripts.load_images as li
+import scripts.layout as ly
 
 character_config_folder = "resources/characters/"
 
@@ -15,6 +16,8 @@ class Character:
         self.name = name
         self.x = x
         self.y = y
+        self.animation = 0
+        self.frame = 0
         self.actions = {}
         parser = ConfigParser()
         parser.read(character_config_folder+config)
@@ -24,10 +27,33 @@ class Character:
         acts = parser.get('character','actions').split('\n')
         for a in acts:
             anim_name = parser.get(a,'sheet')+'.png'
-            self.actions[a]=li.load_animations(anim_name,80)        "returns the position in the map of this character"
+            self.actions[a]=li.load_animations(anim_name,ly.tilesize)
+    
+    def draw(self):
+        """
+        the function to get the current sprite
+        """
+        rval = None
+        self.frame += 1
+        if self.animation:
+            rval = self.actions[self.animation][self.frame]
+            if self.frame >= len(self.actions[self.animation]):
+                self.frame = 0
+                self.animation = 0
+        else:
+            if ('idle' in self.actions) and (self.frame == 30):
+                self.animate('idle')
+            rval = self.sprite()
+    
+    def get_postion(self):
+        "returns the position in the map of this character"
         return (self.x,self.y)
     
     def get_location(self):
         "returns the location in the image of this character"
         return (self.x*ly.tilesize,self.y*ly.tilesize)
     
+    def animate(self, action):
+        if action in self.actions and not self.animation:
+            self.animation = action
+            self.frame = -1
