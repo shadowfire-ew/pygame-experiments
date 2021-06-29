75775af155f5eafc47e7870436b4ff3afdb3a41a
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-29 16:41:46 -0400

    rounding out the initializer so that the character loads its animations

--- scripts/character.py
+++ scripts/character.py
@@ -1,10 +1,25 @@
 """
 this file sill describe the general character class
 """
+from configparser import ConfigParser
+import pygame
+import scripts.load_images as li
+
+character_config_folder = "resources/characters/"
 
 class Character:
     """
     the character object
     """
-    def __init__(self,name):
-        pass
+    def __init__(self,name, config):
+        self.name = name
+        self.actions = {}
+        parser = ConfigParser()
+        parser.read(character_config_folder+config)
+        sprite_name = parser.get('character','single')
+        print('"'+sprite_name+'"')
+        self.sprite = pygame.image.load(li.location+'sprites/characters/'+sprite_name+'.png')
+        acts = parser.get('character','actions').split('\n')
+        for a in acts:
+            anim_name = parser.get(a,'sheet')+'.png'
+            self.actions[a]=li.load_animations(anim_name,80)
