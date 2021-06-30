242d36ab48107a1caf939d994e81d3502d2dc723
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-29 20:13:22 -0400

    added lists of all object types to manifest
    using a py file as the manifest probably isn't the best way to this, but i imagine it isn't the worst way either

--- scripts/manifest.py
+++ scripts/manifest.py
@@ -5,3 +5,7 @@ these values are shared among many files
 tilesize = 80
 grid = gwidth,gheight = 16,12
 size = width,height = (gwidth*tilesize),(gheight*tilesize)
+
+character_types = ["dev","dev2"]
+item_types = []
+env_object_types = []
