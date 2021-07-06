c3c75c7a53c7df1b90dc75dfa78439608fa40a73
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-07-06 17:47:54 -0400

    some comments for clarity

--- scripts/manifest.py
+++ scripts/manifest.py
@@ -4,25 +4,36 @@ these values are shared among many files
 """
 from scripts.levels import Level
 
+# layout features
+# size of tiles in pixels
 tilesize = 80
+# the grid's size in tiles
 grid = gwidth,gheight = 16,12
+# the width of the inentory bar in tiles
 invBarWidth = 4
+# the width of the margin on both sides of the inventory bar, in pixels
 invBarMargin = 10
 # the overall size of the window in pixels
 size = width,height = (gwidth*tilesize)+(invBarWidth*tilesize)+(invBarMargin*2),(gheight*tilesize)
 
+# object types
 character_types = ["dev1","dev2","player"]
 item_types = []
 env_object_types = []
 
+# system framerate
+# calculations are framerate-dependant
 framerate = 30
 
+# storage of some globally accessed features, such as the level
 level = Level()
 objects = None
 player = None
+# a helper function
 def check_chars_pos(x,y,name):
     print(x,y)
-    # default return of true
+    # default return of true, meaning this space is empty
+    # will eventually do some logic for different object types, like items and teleporters
     rval = True
     if name != "Player" and player.x == x and player.y == y:
         # if the player is in the position
