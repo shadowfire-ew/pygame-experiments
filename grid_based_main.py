8adad391d79b421bfd8ca2c8d382517d78aecee7
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-07-07 14:33:09 -0400

    following new parameters for pathfinder function

--- grid_based_main.py
+++ grid_based_main.py
@@ -61,7 +61,7 @@ def main():
                 mx,my = pygame.mouse.get_pos()
                 dx,dy = mx//80,my//80
                 if 0<=dx<mf.gwidth and 0<=dy<mf.gheight:
-                    print(player.find_path(dx,dy))
+                    print(player.find_path((dx,dy)))
                 
 
             # checking if we have our exit event
