e9123bb2eacd240860cd7016179797cea470def5
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-07-06 20:58:27 -0400

    some minor changes to the node class

--- scripts/Astar.py
+++ scripts/Astar.py
@@ -17,14 +17,24 @@ class Node:
     def taxi_to(self,x,y):
         """
         returns the taxicab distance to a given destination
-        this is our heuristic
+        this is our heuristic (h)
         """
         return abs(self.x-x)+abs(self.y-y)
     def path_to(self):
+        """
+        returns a list of co-ordinate pairs
+        """
         if self.parent is not None:
             return self.parent.path_to()+[(self.x,self.y)]
         else:
             return [(self.x,self.y)]
+    def get_f(self,x,y):
+        """
+        our f function
+        """
+        return self.cost + self.taxi_to(x,y)
+    def __eq__(self, o: object) -> bool:
+        return self.x == o.x and self.y == o.y
 
 class Priority_Queue:
     def __init__(self,object,priority):
