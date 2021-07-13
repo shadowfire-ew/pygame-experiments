8b3c8e5c3dd87215a4fe5b1be7c4b642a00dc421
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-07-13 15:23:22 -0400

    Create environment_objects.py

---
+++ scripts/environment_objects.py
@@ -0,0 +1,13 @@
+"""
+holds the environment objects and associated code
+"""
+
+from scripts.objects import GameObject
+
+
+class EnvironmentObject(GameObject):
+    """
+    the environment object class
+    """
+    def __init__(self,name,config,x=0,y=0,target=None):
+        pass
