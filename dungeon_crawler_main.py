daed9236e2fa337d43075572751237a875cab196
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-12-30 17:20:03 -0500

    outlining idea for dungeon loading

--- dungeon_crawler_main.py
+++ dungeon_crawler_main.py
@@ -1,3 +1,45 @@
 """
 something to test out my dungeon crawler concept
+
+because of the way rooms are generated, 
+over-all dungeon layouts will not make sense in euclidean space
 """
+
+from scripts.dungeon_room import DungeonRoom
+
+
+class DungeonGame:
+    """
+    the class that controlls the game
+    """
+    def __init__(self):
+        self._startRoom = DungeonRoom("starter")
+        self._playerRoom = self._startRoom
+        self._startRoom.LoadRoom()
+    
+    def Draw(self):
+        """
+        draws the room and objects within
+        """
+        roomim = self._playerRoom
+
+    def GenerateNeighbors(self):
+        """
+        randomly generates the neighbor rooms
+        sets the cardinal rooms of the current room object
+        as informed by the details of the room
+        """
+
+    def ChangeRoom(self,direction):
+        """
+        lets player enter the next room
+        """
+        oldroom = self._playerRoom
+        try:
+            self._playerRoom = self._playerRoom.exits[direction]
+        except KeyError:
+            print("room unavailable")
+            return
+        self._playerRoom.LoadRoom()
+        oldroom.DeLoadRoom()
+        self.GenerateNeighbors()
--- scripts/dungeon_room.py
+++ scripts/dungeon_room.py
@@ -1,3 +1,55 @@
 """
 the dungeon room object
 """
+
+class DungeonRoom:
+    """
+    the dungeon room object
+    initiated with roomID
+
+    Functions:
+    - LoadRoom()
+    - DeLoadRoom()
+
+    has a publically accessible dictionary of exits
+    """
+    exits = {}
+    
+    def __init__(self,roomID):
+        """
+        roomId is used for loading
+        """
+        self._rID = roomID
+    
+    def LoadRoom(self):
+        """
+        loads the room image and details to memory
+        """
+        self._details = {}
+        self._im = None
+    
+    def DeLoadRoom(self):
+        """
+        de-loads the room objects and details from memory
+        without deleting the room
+        """
+        self._details = {}
+        self._im = None
+
+    def GetDetail(self,key):
+        """
+        returns the value of the internal details dictionary
+        """
+        return self._details[key]
+    
+    def GetImage(self):
+        """
+        returns the image of the room
+        """
+        return self._im
+
+    def IsLoaded(self):
+        """
+        returns true if the room is already loaded
+        """
+        return self._im is not None
